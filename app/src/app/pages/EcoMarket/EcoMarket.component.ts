3e95bf77a23d4c5e0250292eff0a40a9a5074a3a
Adelya <adelya6387240@gmail.com>
2022-04-23 16:39:29 +0300

    Add new arrays on EcoMarketPage

--- app/src/app/pages/EcoMarket/EcoMarket.component.ts
+++ app/src/app/pages/EcoMarket/EcoMarket.component.ts
@@ -10,10 +10,40 @@ import {DataItems} from '../../stores/productStore'
 })
 
 export class EcoMarketComponent implements OnInit {
-  @Input() data: Array<any>;
+
+  @Input() data_cards: Array<any>;
+  @Input() gender: Array<any>;
+  @Input() type: Array<any>;
+  @Input() brand: Array<any>;
+  @Input() allTp: Array<any>;
+  @Input() allBr: Array<any>;
 
   constructor() {
-    this.data = DataItems;
+    this.data_cards = DataItems;
+    this.gender = [
+      {title: "Мужской", checked: false},
+      {title: "Женский", checked: false},
+    ];
+    this.type = [
+      {title: "Одежда", checked: false},
+      {title: "Обувь", checked: false},
+      {title: "Аксессуары", checked: false},
+    ];
+    this.brand = [
+      {title: "H&M", checked: false},
+      {title: "P&B", checked: false},
+      {title: "Adidas", checked: false},
+      {title: "Nike", checked: false},
+      {title: "Reebok", checked: false},
+      {title: "Puma", checked: false},
+    ];
+    this.allTp = [
+      {title: "Выбрать все", checked: false}
+    ];
+    this.allBr = [
+      {title: "Выбрать все", checked: false}
+    ]
+
   }
 
   ngOnInit(): void {
