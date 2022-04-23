6961a11aa9a0c6135d605bf30fd3d573741d56f4
Adelya <adelya6387240@gmail.com>
2022-04-23 12:00:23 +0300

    Add DataItems

---
+++ app/src/app/stores/productStore.ts
@@ -0,0 +1,72 @@
+export const DataItems = [
+    {
+        price: '1000',
+        brand: 'NIKE',
+        nameDesc: 'Nike Air Max 2021',
+        category: 'Мужская обувь',
+        picture: '../../assets/images/nike1.png',
+    },
+    {
+        price: '750',
+        brand: 'NIKE',
+        nameDesc: 'Nike Air Max 90 Premium',
+        category: 'Мужская обувь',
+        picture: '../../assets/images/nike2.png'
+    },
+    {
+        price: '1200',
+        brand: 'Adidas',
+        nameDesc: 'Adidas Alphabounce RC',
+        category: 'Мужская обувь',
+        picture: '../../assets/images/nike3.png'
+    },
+    {
+        price: '1000',
+        brand: 'H&M',
+        nameDesc: 'H&M худи',
+        category: 'Мужская одежда',
+        picture: '../../assets/images/nike4.png'
+    },
+    {
+        price: '2100',
+        brand: 'NIKE',
+        nameDesc: 'Nike Air Force 1 Low',
+        category: 'Мужская обувь',
+        picture: '../../assets/images/nike5.png'
+    },
+    {
+        price: '1000',
+        brand: 'NIKE',
+        nameDesc: 'Nike Air Max 2021',
+        category: 'Мужская обувь',
+        picture: '../../assets/images/nike1.png',
+    },
+    {
+        price: '750',
+        brand: 'NIKE',
+        nameDesc: 'Nike Air Max 90 Premium',
+        category: 'Мужская обувь',
+        picture: '../../assets/images/nike2.png'
+    },
+    {
+        price: '1200',
+        brand: 'Adidas',
+        nameDesc: 'Adidas Alphabounce RC',
+        category: 'Мужская обувь',
+        picture: '../../assets/images/nike3.png'
+    },
+    {
+        price: '1000',
+        brand: 'H&M',
+        nameDesc: 'H&M худи',
+        category: 'Мужская одежда',
+        picture: '../../assets/images/nike4.png'
+    },
+    {
+        price: '2100',
+        brand: 'NIKE',
+        nameDesc: 'Nike Air Force 1 Low',
+        category: 'Мужская обувь',
+        picture: '../../assets/images/nike5.png'
+    }
+]
