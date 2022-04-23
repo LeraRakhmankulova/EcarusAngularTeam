c8b7ce12b862e558cd268a798af79def37d36500
Adelya <adelya6387240@gmail.com>
2022-04-23 16:20:45 +0300

    Add array with cards and working with them

--- app/src/app/pages/EcoMarket/EcoMarket.component.ts
+++ app/src/app/pages/EcoMarket/EcoMarket.component.ts
@@ -1,5 +1,6 @@
 import './scroll_custom.css'
-import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
+import {ChangeDetectionStrategy, Component, Input, OnInit} from '@angular/core';
+import {DataItems} from '../../stores/productStore'
 
 @Component({
   selector: 'app-eco-market',
@@ -9,7 +10,10 @@ import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
 })
 
 export class EcoMarketComponent implements OnInit {
+  @Input() data: Array<any>;
+
   constructor() {
+    this.data = DataItems;
   }
 
   ngOnInit(): void {
