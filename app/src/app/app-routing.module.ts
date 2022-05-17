33200669691b38ccabf8a76ff180838bc09bc7dc
LeraRakhmankulova <lerakhmankulova@mail.ru>
2022-05-18 00:32:22 +0300

    fix inner router

--- app/src/app/app-routing.module.ts
+++ app/src/app/app-routing.module.ts
@@ -1,39 +1,51 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {MainComponent} from "./pages/Main/main.component";
-import {EcoMarketComponent} from "./pages/EcoMarket/EcoMarket.component";
-import {MapComponent} from "./components/map/Map.component";
-import {FooterComponent} from "./components/footer/footer.component.";
+import { MainComponent } from "./pages/Main/main.component";
+import { EcoMarketComponent } from "./pages/EcoMarket/EcoMarket.component";
+import { FooterComponent } from "./components/footer/footer.component.";
 import { ProfilComponent } from './pages/Profil/ProfilPage.component';
 import { MapPageComponent } from './pages/MapPage/MapPage.component';
+import { HistoryCardComponent } from './components/cards/history-card/history-card.component';
+import { UserpromoCardComponent } from './components/cards/userPromo-card/userPromo-card.component';
+
+
+const profileRoutes: Routes = [
+  {
+    path: 'promocode',
+    component: UserpromoCardComponent
+  },
+  {
+    path: 'history',
+    component: HistoryCardComponent
+  }
+]
 
 const routes: Routes = [
   {
     path: '',
     component: MainComponent,
-    pathMatch:'full'
+    pathMatch: 'full'
   },
   {
-    path:'collection_points',
+    path: 'collection_points',
     component: MapPageComponent,
-    pathMatch:'full'
+    pathMatch: 'full'
   },
   {
-    path:'eco_market',
+    path: 'eco_market',
     component: EcoMarketComponent,
-    pathMatch:'full'
+    pathMatch: 'full'
   },
   {
-    path:'about_service',
+    path: 'about_service',
     component: FooterComponent,
-    pathMatch:'full'
+    pathMatch: 'full'
   },
   {
-    path:'profil',
+    path: 'profil',
     component: ProfilComponent,
-    pathMatch:'full'
+    children: profileRoutes
   }
-
 ];
 
 @NgModule({
