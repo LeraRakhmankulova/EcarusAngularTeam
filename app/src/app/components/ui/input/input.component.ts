453724b7be2bd0dbea4c5a4d0fef6a5ba08ebd1f
LeraRakhmankulova <lerakhmankulova@mail.ru>
2022-05-19 11:52:35 +0300

    front fix

--- app/src/app/components/ui/input/input.component.ts
+++ app/src/app/components/ui/input/input.component.ts
@@ -5,25 +5,17 @@ import { Component, Input, OnInit } from '@angular/core';
   templateUrl: './input.component.html',
   styleUrls: ['./input.component.sass']
 })
-export class InputComponent implements OnInit {
+export class InputComponent {
   @Input() placeholder: string;
   @Input() type: string;
   @Input() name: string;
   @Input() formName: string;
-  // @Input() onBlur: string;
-  // @Input() value: string;
 
   constructor() {
     this.placeholder = "";
     this.type = "";
     this.name = "";
     this.formName="";
-    // this.onChange="";
-    // this.onBlur="";
-    // this.value='';
-  }
-
-  ngOnInit(): void {
   }
 
 }
