74a0dd6d3cafca8954c1b673ab71af5b88d5293a
Adelya <adelya6387240@gmail.com>
2022-05-21 12:05:01 +0300

    Add profile with api

--- app/src/app/pages/Profil/ProfilPage.component.ts
+++ app/src/app/pages/Profil/ProfilPage.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
+import {DialogRef} from "@angular/cdk-experimental/dialog";
+import {DialogService} from "@services/dialog.service";
+import {FormBuilder} from "@angular/forms";
+import {ToastService} from "@services/toast.service";
+import {AuthService} from "@services/auth.service";
+import {User} from "@utils/local-storage";
 
 @Component({
   selector: 'app-profil',
   templateUrl: './ProfilPage.component.html',
-  styleUrls: ['./ProfilPage.component.sass']
+  styleUrls: ['./ProfilPage.component.sass'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ProfilComponent implements OnInit {
-
-  constructor() { }
+  constructor(
+              private dialog: DialogService,
+              private fb: FormBuilder,
+              private toast: ToastService,
+              private authService: AuthService) { }
 
   ngOnInit(): void {
   }
-
+  getProfile(): void {
+    this.authService.getProfile().subscribe(res => {
+      console.log(res)
+    });
+  }
 }
