182ddd1ae71dc7cbb5be38201cb47aba711b4cb5
LeraRakhmankulova <lerakhmankulova@mail.ru>
2022-05-26 17:37:45 +0300

    menu update

--- app/src/app/components/modal/menu-modal/menu.component.ts
+++ app/src/app/components/modal/menu-modal/menu.component.ts
@@ -1,6 +1,8 @@
 import { DialogRef } from '@angular/cdk-experimental/dialog';
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy, Input } from '@angular/core';
 import { AuthService } from '@services/auth.service';
+import { DialogService } from '@services/dialog.service';
+import { SignModalComponent } from '../sign-modal/sign-modal.component';
 
 @Component({
   selector: 'app-menu',
@@ -11,8 +13,10 @@ import { AuthService } from '@services/auth.service';
 export class MenuComponent implements OnInit {
   user: any;
 
-  constructor(private authService: AuthService,  
-    private dialogRef: DialogRef<MenuComponent>) {}
+  constructor(public authService: AuthService,  
+    private dialogRef: DialogRef<MenuComponent>,
+    private dialog: DialogService) {
+    }
 
   ngOnInit(): void {
     this.authService.getProfile().subscribe(res => {
@@ -21,5 +25,12 @@ export class MenuComponent implements OnInit {
   }
   close(resolve: boolean): void {
     this.dialogRef.close(resolve);
+  }
+  openActionDialog() {
+    const dialog = this.dialog.openDialog(SignModalComponent, {
+      data: {
+        name: 'Иван Иванович'
+      },
+    })
   }
 }
